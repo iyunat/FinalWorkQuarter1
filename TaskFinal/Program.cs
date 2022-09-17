acb1baa1b51af548887d077b8cd468e57d832575
Natalya <iyunat@yandex.ru>
2022-09-17 23:52:16 +0300

    Creating Description.md and Result.drawio

--- TaskFinal/Program.cs
+++ TaskFinal/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк 
+//формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+string[] array1 = new string[5] {"Финал", "Четверть 1", "Ура", "Идем дальше", "!"};
+string[] array2 = new string[array1.Length];
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+NewArray(array1, array2);
+PrintArray(array2);
